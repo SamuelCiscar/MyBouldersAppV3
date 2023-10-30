95d57405a26b505e04f55cfb1b6e8d27deee5e5e
SamuelCiscar <133649133+SamuelCiscar@users.noreply.github.com>
2023-10-30 10:08:10 +0100

    azerty

---
+++ app/src/main/java/com/example/mybouldersapp/ProfilActivity.java
@@ -0,0 +1,60 @@
+package com.example.mybouldersapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0, "Accueil");
+        menu.add(0,2,0, "Météo");
+        menu.add(0,3,0, "Map");
+        menu.add(0,3,0, "Mes Blocs");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == 1){
+            Intent intent = new Intent(this, SecondActivity.class);
+
+            startActivity(intent);
+
+            finish();
+        } else if (item.getItemId() == 2) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+
+            startActivity(intent);
+
+            finish();
+        } else if (item.getItemId() == 3) {
+            Intent intent = new Intent(this, MapActivity.class);
+
+            startActivity(intent);
+
+            finish();
+
+        } else if (item.getItemId() == 4) {
+            Intent intent = new Intent(this, BlocsActivity.class);
+
+            startActivity(intent);
+
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
